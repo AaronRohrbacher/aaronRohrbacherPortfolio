01f0d899ba8d98bed15d8a3b1893012394db421c
Aaron Rohrbacher <arjazz@icloud.com>
2025-02-09 14:29:15 -0800

    adds portfolio descriptions, attempts to space them, and adds a new mockup image for my tired old brain to use with GIMP

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import IconLink from "./IconLink";
-import {Box} from "@mui/material";
+import IconLink from './IconLink';
+import { Box } from '@mui/material';
 
 function PortfolioBlock(props) {
-   const {image, live, source, title} = props;
+   const { image, desc, source, title } = props;
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
-         <h1 style={{fontSize: '2rem'}}>{title}</h1>
+         <Box component={'img'} src={image} alt={'mockup'} />
+         <h1 style={{ fontSize: '2rem' }}>{title}</h1>
+         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100px' }}>
+            <p style={{ margin: '0', fontSize: '20px', textAlign: 'center', maxWidth: '450px'}}>{desc}</p>
+         </div>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
-              alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
+            alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
-            </Box>
-            <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
+               <IconLink link={source} title={'Source Code'} icon={'fa fa-code'} />
             </Box>
          </Box>
       </Box>
--- src/info/Info.js
+++ src/info/Info.js
@@ -2,39 +2,19 @@ import self from "../img/self.png"
 import site from "../img/site.png"
 import klear from "../img/klear.png"
 import thinger from "../img/thinger.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
-
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-/* 
-This variable will change the layout of the website from multipage to single, scrollable page
-*/
 export let singlePage = false;
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
     firstName: "Aaron",
     lastName: "Rohrbacher",
-    initials: "ar", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "ar",
     position: "a Senior Full Stack Cloud Engineer",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    selfPortrait: self,
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`,
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [
         {
             emoji: '🎷',
             text: 'and a musician'
@@ -47,10 +27,6 @@ export const info = {
             emoji: "💼",
             text: "Technical Lead and Senior Software Engineer"
         },
-        // {
-        //     emoji: "📧",
-        //     text: "thinger"
-        // }
     ],
     socials: [
         {
@@ -63,9 +39,6 @@ export const info = {
             icon: "fa fa-linkedin",
             label: 'linkedin'
         },
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
-
     ],
     bio: "Hi there! My name is Aaron Rohrbacher, and I'm a senior-level full stack software engineer. I'm currently working for a company named Sparq, where I drive our clients' most critical infrastructure decision making. Using the aforementioned decisions, I architect comprehensive cloud infrastructure, and lead my teams as a player-coach in building web apps that our clients fall in love with. I thrive on doing the right thing, and those who do the same! When I'm not behind a computer keyboard, I'm behind a musical one, or playing my saxophone.",
     skills:
@@ -73,33 +46,34 @@ export const info = {
             proficientWith: ['AWS, Azure, and Google Cloud Platform', 'Python', 'NodeJS', 'Java', 'Ruby, Ruby on Rails', 'SQL (PostgreSQL, MySQL and derivatives)', 'NoSql Databases (DynamoDB, Mongo)', 'Bash', 'Powershell', 'And so much more...' ],
         }
     ,
-// Same as above, change the emojis to match / relate to your hobbies or interests.
-// You can also remove the emojis if you'd like, I just think they look cute :P
-    portfolio: [ // This is where your portfolio projects will be detailed
+    portfolio: [ 
         {
             title: "Developer Website",
-            live: "https://aaronrohrbacher.com/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/AaronRohrbacher/aaronRohrbacherPortfolio", // this should be a link to the **repository** of the project, where the code is hosted.
+            desc: "A fork of Payton Jewell's well-done React design, used to refactor for this website!",
+            source: "https://github.com/AaronRohrbacher/aaronRohrbacherPortfolio",
             image: site
         },
         {
             title: "Klear",
-            live: "https://store.kde.org/p/2216605",
+            desc: "A simple implementation of KDE Plasma's JavaScript API for Kwin, allowing for transparent windows on older hardware.",
             source: "https://github.com/AaronRohrbacher/klear_kwin",
             image: klear
         },
         {
             title: "Thinger",
-            source: "https://github.com/paytonjewell",
+            desc: "A Python script that isolates the use of a given word or phrase in a video, and compiles all uses of that word to a new video. As the kids say, it's keen!",
+            source: "https://github.com/AaronRohrbacher/thinger",
             image: thinger
         },
         {
             title: "ai",
+            desc: "A PyTorch implementation of the GPT-2 LLM, trained to respond to prompts based on transcriptions of George Carlin performances. WARNING: it's utterly vile!",
             source: "https://github.com/AaronRohrbacher/ai",
             image: thinger
         },
         {
             title: "Nuel API",
+            desc: "A small example from proprietary software I wrote that provisions LAMP stacks and other resources to client accounts, automates billing and signup, and provides a HUGELY fast PHP experience.",
             source: "https://github.com/AaronRohrbacher/nuel-api",
             image: thinger
         }
