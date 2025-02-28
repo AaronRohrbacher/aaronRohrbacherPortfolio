7b6ca5e26e720668f04999d2a7ab4e2268c6583f
Aaron Rohrbacher <arjazz@icloud.com>
2025-02-28 06:47:20 -0800

    adds resume, new images of my stupid face

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,19 +1,30 @@
-import { Navigate } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import aaron_rohrbacher from './aaron_rohrbacher.pdf';
 
 function Resume() {
-    return (
-        <div>
-            <a
-                href="https://aaronrohrbacher-com.s3.us-west-2.amazonaws.com/dtetravel_com_instructions.pdf"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ display: 'none' }}
-                id="pdfLink"
-                ref={el => el && el.click()}
-            />
-            <Navigate to={'/'} />
-        </div>
-    );
+  const containerRef = useRef(null);
+  
+  useEffect(() => {
+    const adjustHeight = () => {
+      if (containerRef.current) {
+        containerRef.current.style.height = `${window.innerHeight}px`;
+      }
+    };
+    
+    adjustHeight();
+    
+    window.addEventListener('resize', adjustHeight);
+    
+    return () => {
+      window.removeEventListener('resize', adjustHeight);
+    };
+  }, []);
+  
+  return (
+    <div ref={containerRef} style={{ overflow: 'hidden' }}>
+      <object data={aaron_rohrbacher} type="application/pdf" width="100%" height="100%" style={{ display: 'block' }} />
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
